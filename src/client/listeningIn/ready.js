1f2e0db551508dfb40c3734659f636a83e845de8
Eduardo <111902860+Edu-213@users.noreply.github.com>
2022-08-23 15:35:26 -0300

    Update ready.js

--- src/client/listeningIn/ready.js
+++ src/client/listeningIn/ready.js
@@ -1,7 +1,5 @@
 const Event = require('../../structures/Event');
-const { ActivityType } = require('discord.js');
 const c = require('colors')
-const cfonts = require('cfonts');
 
 module.exports = class ready extends Event {
     constructor(client) {
@@ -11,19 +9,7 @@ module.exports = class ready extends Event {
     }
 
     run = async () => {
-        /*
-        const banner = cfonts.render((`Estou on!`), {
-            font: 'block',
-            color: 'candy',
-            align: 'left',
-            gradient: ["red","magenta"],
-            lineHeight: 3
-        });
-    console.log(banner.string); 
-    */
-        console.log(c.red(`✅ ${this.client.user.username} logado com sucesso em ${this.client.guilds.cache.size} servidores!`))
-        //this.client.user.setPresence({ activities: ({ name: 'aaaaaaaaaa', type: ActivityType.Watching })}) //defina o name para aparecer na presence do bot.
-        //this.client.user.setStatus('online') //status do bot, veja no discord.js as outras opções
+        console.log(c.red(`ONLINE`))
         this.client.registryCommands()
 
     }
