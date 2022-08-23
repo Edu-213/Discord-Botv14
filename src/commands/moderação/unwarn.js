2b16951b89641eaafea2efaaa3c39da2b2838927
Eduardo <111902860+Edu-213@users.noreply.github.com>
2022-08-23 20:09:36 -0300

    Update unwarn.js

--- src/commands/moderação/unwarn.js
+++ src/commands/moderação/unwarn.js
@@ -1,25 +1,26 @@
 const Command = require('../../structures/Command');
-const { ActionRowBuilder, EmbedBuilder, ButtonBuilder, InteractionResponse  } = require("discord.js");
 const {salvatudo} = require("../../funcoes/funcoes");
 const warnModel = require("../../database/Schemas/warnModel");
-const Discord = require('discord.js')
+const ClientEmbed = require("../../utils/ClientEmbed");
+const Emojis = require("../../utils/Emojis");
+
 
 module.exports = class unwarn extends Command {
     constructor(client) {
         super(client, {
             name: 'unwarn',
-            description: '[Moderação] Retirar a advertencia de um membro',
+            description: 'Retirar a advertência de um membro',
             options: [
                 {
                     name: 'membro',
                     type: 6,
-                    description: 'Coloque o usuário para retirar a advertencia.',
+                    description: 'Coloque o usuário para retirar a advertência.',
                     require: true,
                 },
                 {
                     name: 'advertencia',
                     type: 4,
-                    description: 'Cite o numero da advertencia que queira retirar',
+                    description: 'Cite a advertência que queira retirar',
                     require: true
                 }
             ]
@@ -27,7 +28,8 @@ module.exports = class unwarn extends Command {
     }
 
     run = async (interaction) => {
-        if (!interaction.member.permissions.has(Discord.PermissionFlagsBits.KickMembers)) return interaction.reply({content: `${interaction.user}, Você não possui a permissão \`Expulsar membros\` para poder utilizar este comando.`, ephemeral: true})
+        if (!interaction.member.permissions.has("KickMembers")) return interaction.reply({content: `${interaction.user}, Você não possui a permissão \`Expulsar membros\` para poder utilizar este comando.`, ephemeral: true})
+        
         let user = interaction.options.getMember('membro');
         let num = interaction.options.getInteger('advertencia')
         
@@ -37,15 +39,15 @@ module.exports = class unwarn extends Command {
 
         let data = await warnModel.findOne({ guild: interaction.guild.id, user: user.id });
 
-        if (user.id == interaction.user.id) return interaction.reply({content: `${interaction.user}, você não pode retirar advertencia de si mesmo.`, ephemeral: true});
+        if (user.id == interaction.user.id) return interaction.reply({content: `${interaction.user}, você não pode retirar advertência de si mesmo.`, ephemeral: true});
         if (user.roles.highest.position) return interaction.reply({content: `${interaction.user}, Eu não posso punir este membro, meu cargo e menor que o do membro a ser punido!`, ephemeral: true});
 
-        if (data.warnings.length === 0) return interaction.reply(`${interaction.user} O membro especificado não tem nenhuma advertência!`);
-        if (isNaN(id_warn) || id_warn < 0) return interaction.reply(`${interaction.user} A advertência especificada não e válida!`);
-        if(data.warnings[id_warn] == undefined) return interaction.reply(`${interaction.user} O membro tem menos que **${num}** advertências! Utilize, \`/warnlist\` para ver a lista de advertência de um membro.`);
+        if (data.warnings.length === 0) return interaction.reply({content: `${interaction.user} O membro especificado não tem nenhuma advertência!`, ephemral: true});
+        if (isNaN(id_warn) || id_warn < 0) return interaction.reply({content: `${interaction.user} A advertência especificada não e válida!`, ephemeral: true});
+        if(data.warnings[id_warn] == undefined) return interaction.reply({content: `${interaction.user} O membro tem menos que **${num}** advertências! Utilize, \`/warnlist\` para ver a lista de advertência de um membro.`, ephemeral: true});
 
-       let warn = new EmbedBuilder()
-        .setTitle(` | Advertência Removida`)
+       let warn = new ClientEmbed()
+        .setTitle(`${Emojis.adv} Advertência Removida`)
         .setDescription(`${interaction.user}, O membro teve uma advertência removida!`)
         interaction.reply({embeds: [warn]})
         data.warnings.splice(id_warn, 1);
