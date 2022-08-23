67e223387633724420d5452298f6d63eba71c6f1
Eduardo <111902860+Edu-213@users.noreply.github.com>
2022-08-23 15:37:39 -0300

    Update ping.js

--- src/commands/information/ping.js
+++ src/commands/information/ping.js
@@ -1,16 +1,14 @@
 const Command = require('../../structures/Command');
-const { ApplicationCommandType } = require('discord.js');
 
 module.exports = class ping extends Command {
     constructor(client) {
         super(client, {
             name: 'ping',
             description: 'mostra o ping do bot',
-            type: ApplicationCommandType.ChatInput
         })
     }
 
     run = async (interaction) => {
-        interaction.reply(`O ping do bot é \`${this.client.ws.ping}\` ms`)
+        interaction.reply(`O meu ping é \`${this.client.ws.ping}\` ms`)
     }
 }
